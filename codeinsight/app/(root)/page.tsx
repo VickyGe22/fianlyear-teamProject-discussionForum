edd6b014eabeb5f5aae9970c085020189d6bbc3b
lancy-buding <zlx970515@126.com>
2024-04-02 19:20:15 +1030

    update the router

--- codeinsight/app/(root)/page.tsx
+++ codeinsight/app/(root)/page.tsx
@@ -13,6 +13,9 @@ import Link from 'next/link'
 
 
 export default function Home() {
+
+
+
   return (
     <>
       <section>
@@ -24,6 +27,7 @@ export default function Home() {
       <section>
         <br></br>
         <Intro /> {/* Make sure the Intro component does not have excessive bottom margin or padding */}
+
           <div className="flex flex-wrap justify-center gap-4 py-2 md:py-4 drop-shadow-2xl">
             <div className="rounded-md overflow-hidden relative" style={{ width: 'calc(32% - 1rem)', height: '500px' }}>
               <Image src={submit} alt="submits" layout="fill" objectFit="cover" />
@@ -38,7 +42,7 @@ export default function Home() {
           <div className="rounded-md overflow-hidden relative" style={{ width: 'calc(32% - 1rem)', height: '500px' }}>
             <Image src={discussion} alt="discussion" layout="fill" objectFit="cover" />
             <div className="absolute inset-0 flex flex-col justify-start items-center pt-40">
-                <Link href="/discussion-forum">
+                <Link href="/code-sample-repository">
                     <button className="inline-flex items-center rounded-lg bg-indigo-500 hover:bg-indigo-600 px-4 py-2 text-sm font-bold text-white shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500/50 focus:ring-offset-2 focus:ring-offset-white" style={{ cursor: 'pointer' }}>
                       Code Issue Discussion Forum
                     </button>
@@ -50,7 +54,7 @@ export default function Home() {
             <div className="absolute inset-0 flex flex-col justify-start items-center pt-40">
                 <Link href="/review-page">
                     <button className="inline-flex items-center rounded-lg bg-indigo-500 hover:bg-indigo-600 px-4 py-2 text-sm font-bold text-white shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500/50 focus:ring-offset-2 focus:ring-offset-white" style={{ cursor: 'pointer' }}>
-                      Reviwe Your Code Sample
+                      Review Your Code Sample
                     </button>
                 </Link>
             </div>
