fde953614bc260bc7e195df0798ade725de22a63
lancy-buding <zlx970515@126.com>
2024-04-03 21:20:08 +1030

    change the interface of menubox

--- codeinsight/app/(root)/submits/menubox.tsx
+++ codeinsight/app/(root)/submits/menubox.tsx
@@ -2,28 +2,43 @@
 const MenuBox = () => {
     // Component logic
     return <div>
-        {/*Level*/}
-        <div className="space-y-4">
-        <div>
-        <label className="block text-sm font-medium mb-1" htmlFor="position">
-            Level <span className="text-red-500">*</span>
-        </label>
-        <input id="position" className="form-input w-full" type="text" required placeholder="E.g., Senior Software Engineer" />
-        </div>
-        </div>
-        {/*Languages*/}
-        <div>
-        <label className="block text-sm text-gray-800 font-medium mb-1" htmlFor="role">
-            Languages <span className="text-rose-500">*</span>
-        </label>
-        <select id="role" className="form-select text-sm py-2 w-full" required>
-            <option>Python</option>
-            <option>C</option>
-            <option>C++ / Finance</option>
-        </select>
-        </div>
+       
+        <div className="flex justify-left">
+            {/* Languages */}
+            <div className="flex flex-col">
+                <label className="block text-sm text-gray-800 font-medium mb-1" htmlFor="languages">
+                    Languages <span className="text-rose-500">*</span>
+                </label>
+                <select id="languages" className="form-select text-sm py-2" required>
+                    <option>Python</option>
+                    <option>C</option>
+                    <option>C++ / Finance</option>
+                </select>
+            </div>
+            {/* Level */}
+            <div className="flex flex-col pl-10">
+                <label className="block text-sm text-gray-800 mb-1" htmlFor="level">
+                    Level <span className="text-rose-500">*</span>
+                </label>
+                <select id="level" className="form-select text-sm py-2" required>
+                    <option>Python</option>
+                    <option>C</option>
+                    <option>C++ / Finance</option>
+                </select>
+            </div>
+            {/* Type */}
+            <div className="flex flex-col pl-10">
+                <label className="block text-sm text-gray-800 font-medium mb-1" htmlFor="level">
+                    Type <span className="text-rose-500">*</span>
+                </label>
+                <select id="Type" className="form-select text-sm py-2" required>
+                    <option>Python</option>
+                    <option>C</option>
+                    <option>C++ / Finance</option>
+                </select>
+            </div>
 
-
+        </div>
 
 
 
