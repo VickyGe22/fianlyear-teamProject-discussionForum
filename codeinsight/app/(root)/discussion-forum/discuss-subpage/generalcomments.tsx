bf51943c4d09dd9946ba3dff65cbae9a4bafdd14
JeromeWJQ <132572172+JeromeWJQ@users.noreply.github.com>
2024-04-08 17:52:27 +0930

    Design issue part and general comment part
    second renew discussion part

--- codeinsight/app/(root)/discussion-forum/discuss-subpage/generalcomments.tsx
+++ codeinsight/app/(root)/discussion-forum/discuss-subpage/generalcomments.tsx
@@ -1,13 +1,150 @@
 
 
 
-export default function GeneralComments() {
+// export default function GeneralComments() {
 
-    return(
-        <div className="text-sm text-indigo-500 font-medium hover:underline" >
-            issue
-        </div>
+//     return (
+//         <div className="text-sm text-indigo-500 font-medium hover:underline">
+//           <h2 className="text-lg font-bold mb-2 text-black">General Comment</h2>
+//           <div className="overflow-x-auto">
+//             <table className="table-auto w-full border-collapse">
+//               <thead>
+//                 <tr className="bg-gray-700 text-white rounded-lg">
+//                   <th className="px-4 py-2 border w-2/10 cursor-pointer hover:bg-gray-600">Author</th>
+//                   <th className="px-4 py-2 border w-6/10 cursor-pointer hover:bg-gray-600">Description</th>
+//                   <th className="px-4 py-2 border w-2/10 cursor-pointer hover:bg-gray-600">Reply</th>
+//                 </tr>
+//               </thead>
+//               <tbody>
+//                 <tr className="bg-gray-200  rounded-lg">
+//                   <td className="px-4 py-8 border cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>JACK</td>
+//                   <td className="px-4 py-8 border cursor-pointer hover:bg-gray-300" style={{ width: '60%' }}>XXXXXXXX</td>
+//                   <td className="px-4 py-8 border cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>10∨</td>
+//                 </tr>
+//                 <tr className="bg-gray-200 rounded-lg">
+//                   <td className="px-4 py-8 border w-2/10 cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>Jerome</td>
+//                   <td className="px-4 py-8 border w-6/10 cursor-pointer hover:bg-gray-300" style={{ width: '60%' }}>XXXXXXXXX</td>
+//                   <td className="px-4 py-8 border w-2/10 cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>8∨</td>
+//                 </tr>
+//                 <tr className="bg-gray-200 rounded-lg">
+//                   <td className="px-4 py-8 border w-2/10 cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>Alex</td>
+//                   <td className="px-4 py-8 border w-6/10 cursor-pointer hover:bg-gray-300" style={{ width: '60%' }}>XXXXXXXX</td>
+//                   <td className="px-4 py-8 border w-2/10 cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>6∨</td>
+//                 </tr>
+//               </tbody>
+//             </table>
+//           </div>
+//         </div>
+//       );
+
+// }
+
+import {
+    AcademicCapIcon,
+    BanknotesIcon,
+    CheckBadgeIcon,
+    ClockIcon,
+    ReceiptRefundIcon,
+    UsersIcon,
+  } from '@heroicons/react/24/outline'
+  
+  const actions = [
+    {
+      title: 'JACK',
+      href: '#',
+      icon: UsersIcon,
+      iconForeground: 'text-teal-700',
+      iconBackground: 'bg-teal-50',
+    },
+    {
+      title: 'ALEX',
+      href: '#',
+      icon: UsersIcon,
+      iconForeground: 'text-purple-700',
+      iconBackground: 'bg-purple-50',
+    },
+    {
+      title: 'ARTHUR',
+      href: '#',
+      icon: UsersIcon,
+      iconForeground: 'text-sky-700',
+      iconBackground: 'bg-sky-50',
+    },
+    {
+      title: 'IVORY',
+      href: '#',
+      icon: UsersIcon,
+      iconForeground: 'text-yellow-700',
+      iconBackground: 'bg-yellow-50',
+    },
+    {
+      title: 'JEROME',
+      href: '#',
+      icon: UsersIcon,
+      iconForeground: 'text-rose-700',
+      iconBackground: 'bg-rose-50',
+    },
+    {
+      title: 'SAM',
+      href: '#',
+      icon: UsersIcon,
+      iconForeground: 'text-indigo-700',
+      iconBackground: 'bg-indigo-50',
+    },
+  ]
+  
+  function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+  }
+  
+  export default function Example() {
+    return (
+        <><h1 className="text-2xl font-bold mb-4 text-black">General Comment</h1><div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0">
+            {actions.map((action, actionIdx) => (
+                <div
+                    key={action.title}
+                    className={classNames(
+                        actionIdx === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '',
+                        actionIdx === 1 ? 'sm:rounded-tr-lg' : '',
+                        actionIdx === actions.length - 2 ? 'sm:rounded-bl-lg' : '',
+                        actionIdx === actions.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '',
+                        'group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500'
+                    )}
+                >
+                    <div>
+                        <span
+                            className={classNames(
+                                action.iconBackground,
+                                action.iconForeground,
+                                'inline-flex rounded-lg p-3 ring-4 ring-white'
+                            )}
+                        >
+                            <action.icon className="h-6 w-6" aria-hidden="true" />
+                        </span>
+                    </div>
+                    <div className="mt-8">
+                        <h3 className="text-base font-semibold leading-6 text-gray-900">
+                            <a href={action.href} className="focus:outline-none">
+                                {/* Extend touch target to entire panel */}
+                                <span className="absolute inset-0" aria-hidden="true" />
+                                {action.title}
+                            </a>
+                        </h3>
+                        <p className="mt-2 text-sm text-gray-500">
+                            xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        </p>
+                    </div>
+                    <span
+                        className="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400"
+                        aria-hidden="true"
+                    >
+                        <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
+                        </svg>
+                    </span>
+                </div>
+            ))}
+        </div></>
     )
-
-}
-
+  }
+  
--- codeinsight/app/(root)/discussion-forum/discuss-subpage/issuepage.tsx
+++ codeinsight/app/(root)/discussion-forum/discuss-subpage/issuepage.tsx
@@ -1,38 +1,268 @@
-import React from 'react';
+// import React from 'react';
 
-export default function Issue() {
+// export default function Issue() {
+//   return (
+//     <div className="text-sm text-indigo-500 font-medium hover:underline">
+//       <h2 className="text-lg font-bold mb-2 text-black">ISSUE</h2>
+//       <div className="overflow-x-auto">
+//         <table className="table-auto w-full border-collapse">
+//           <thead>
+//             <tr className="bg-gray-700 text-white rounded-lg">
+//               <th className="px-4 py-2 border w-2/10 cursor-pointer hover:bg-gray-600">ISSUE</th>
+//               <th className="px-4 py-2 border w-6/10 cursor-pointer hover:bg-gray-600">Description</th>
+//               <th className="px-4 py-2 border w-2/10 cursor-pointer hover:bg-gray-600">Reply</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             <tr className="bg-gray-200  rounded-lg">
+//               <td className="px-4 py-8 border cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>For loop</td>
+//               <td className="px-4 py-8 border cursor-pointer hover:bg-gray-300" style={{ width: '60%' }}>XXXXXXXX</td>
+//               <td className="px-4 py-8 border cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>6∨</td>
+//             </tr>
+//             <tr className="bg-gray-200 rounded-lg">
+//               <td className="px-4 py-8 border w-2/10 cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>Binary tree</td>
+//               <td className="px-4 py-8 border w-6/10 cursor-pointer hover:bg-gray-300" style={{ width: '60%' }}>XXXXXXXXX</td>
+//               <td className="px-4 py-8 border w-2/10 cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>18∨</td>
+//             </tr>
+//             <tr className="bg-gray-200 rounded-lg">
+//               <td className="px-4 py-8 border w-2/10 cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>febbonach</td>
+//               <td className="px-4 py-8 border w-6/10 cursor-pointer hover:bg-gray-300" style={{ width: '60%' }}>XXXXXXXX</td>
+//               <td className="px-4 py-8 border w-2/10 cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>13∨</td>
+//             </tr>
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// }
+
+import { ChatBubbleLeftIcon, CheckCircleIcon } from '@heroicons/react/24/outline'
+
+const discussions = [ 
+  {
+    id: 1,
+    title: 'Binary Tree',
+    href: '#',
+    author: { name: 'Leslie Alexander', href: '#' },
+    date: '2d ago',
+    dateTime: '2023-01-23T22:34Z',
+    status: 'active',
+    totalComments: 24,
+    commenters: [
+      {
+        id: 12,
+        name: 'Emma Dorsey',
+        imageUrl:
+          'https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 6,
+        name: 'Tom Cook',
+        imageUrl:
+          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 4,
+        name: 'Lindsay Walton',
+        imageUrl:
+          'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 16,
+        name: 'Benjamin Russel',
+        imageUrl:
+          'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 23,
+        name: 'Hector Gibbons',
+        imageUrl:
+          'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Quick Sort',
+    href: '#',
+    author: { name: 'Michael Foster', href: '#' },
+    date: '2d ago',
+    dateTime: '2023-01-23T19:20Z',
+    status: 'active',
+    totalComments: 6,
+    commenters: [
+      {
+        id: 13,
+        name: 'Alicia Bell',
+        imageUrl:
+          'https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 16,
+        name: 'Benjamin Russel',
+        imageUrl:
+          'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 3,
+        name: 'Dries Vincent',
+        imageUrl:
+          'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'K-Means',
+    href: '#',
+    author: { name: 'Dries Vincent', href: '#' },
+    date: '3d ago',
+    dateTime: '2023-01-22T12:59Z',
+    status: 'resolved',
+    totalComments: 22,
+    commenters: [
+      {
+        id: 19,
+        name: 'Lawrence Hunter',
+        imageUrl:
+          'https://images.unsplash.com/photo-1513910367299-bce8d8a0ebf6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 21,
+        name: 'Angela Fisher',
+        imageUrl:
+          'https://images.unsplash.com/photo-1501031170107-cfd33f0cbdcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 14,
+        name: 'Jenny Wilson',
+        imageUrl:
+          'https://images.unsplash.com/photo-1507101105822-7472b28e22ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 16,
+        name: 'Benjamin Russel',
+        imageUrl:
+          'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Linked List',
+    href: '#',
+    author: { name: 'Lindsay Walton', href: '#' },
+    date: '5d ago',
+    dateTime: '2023-01-20T10:04Z',
+    status: 'resolved',
+    totalComments: 8,
+    commenters: [
+      {
+        id: 10,
+        name: 'Emily Selman',
+        imageUrl:
+          'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 11,
+        name: 'Kristin Watson',
+        imageUrl:
+          'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: 'Hash Table',
+    href: '#',
+    author: { name: 'Courtney Henry', href: '#' },
+    date: '5d ago',
+    dateTime: '2023-01-20T20:12Z',
+    status: 'active',
+    totalComments: 15,
+    commenters: [
+      {
+        id: 11,
+        name: 'Kristin Watson',
+        imageUrl:
+          'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 6,
+        name: 'Tom Cook',
+        imageUrl:
+          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 10,
+        name: 'Emily Selman',
+        imageUrl:
+          'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      {
+        id: 16,
+        name: 'Benjamin Russel',
+        imageUrl:
+          'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+    ],
+  },
+]
+
+export default function Example() {
   return (
-    <div className="text-sm text-indigo-500 font-medium hover:underline">
-      <h2 className="text-lg font-bold mb-2 text-black">ISSUE</h2>
-      <div className="overflow-x-auto">
-        <table className="table-auto w-full border-collapse">
-          <thead>
-            <tr className="bg-gray-700 text-white rounded-lg">
-              <th className="px-4 py-2 border w-2/10 hover:bg-gray-600">ISSUE</th>
-              <th className="px-4 py-2 border w-6/10 hover:bg-gray-600">Description</th>
-              <th className="px-4 py-2 border w-2/10 hover:bg-gray-600">Reply</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="bg-gray-200  rounded-lg">
-              <td className="px-4 py-8 border cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>For loop</td>
-              <td className="px-4 py-8 border cursor-pointer hover:bg-gray-300" style={{ width: '60%' }}>XXXXXXXX</td>
-              <td className="px-4 py-8 border cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>XXXXXXXX</td>
-            </tr>
-            <tr className="bg-gray-200 rounded-lg">
-              <td className="px-4 py-8 border w-2/10 cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>Binary tree</td>
-              <td className="px-4 py-8 border w-6/10 cursor-pointer hover:bg-gray-300" style={{ width: '60%' }}>XXXXXXXXX</td>
-              <td className="px-4 py-8 border w-2/10 cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>XXXXXXXXX</td>
-            </tr>
-            <tr className="bg-gray-200 rounded-lg">
-              <td className="px-4 py-8 border w-2/10 cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>febbonach</td>
-              <td className="px-4 py-8 border w-6/10 cursor-pointer hover:bg-gray-300" style={{ width: '60%' }}>XXXXXXXX</td>
-              <td className="px-4 py-8 border w-2/10 cursor-pointer hover:bg-gray-300" style={{ width: '20%' }}>XXXXXXXXXXXXXXXXX</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+    <><h1 className="text-2xl font-bold mb-4 text-black">Issue</h1><ul role="list" className="divide-y divide-gray-100">
+          {discussions.map((discussion) => (
+              <li
+                  key={discussion.id}
+                  className="flex flex-wrap items-center justify-between gap-x-6 gap-y-4 py-5 sm:flex-nowrap"
+              >
+                  <div>
+                      <p className="text-sm font-semibold leading-6 text-gray-900">
+                          <a href={discussion.href} className="hover:underline">
+                              {discussion.title}
+                          </a>
+                      </p>
+                      <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
+                          <p>
+                              <a href={discussion.author.href} className="hover:underline">
+                                  {discussion.author.name}
+                              </a>
+                          </p>
+                          <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 fill-current">
+                              <circle cx={1} cy={1} r={1} />
+                          </svg>
+                          <p>
+                              <time dateTime={discussion.dateTime}>{discussion.date}</time>
+                          </p>
+                      </div>
+                  </div>
+                  <dl className="flex w-full flex-none justify-between gap-x-8 sm:w-auto">
+                      <div className="flex -space-x-0.5">
+                          <dt className="sr-only">Commenters</dt>
+                          {discussion.commenters.map((commenter) => (
+                              <dd key={commenter.id}>
+                                  <img
+                                      className="h-6 w-6 rounded-full bg-gray-50 ring-2 ring-white"
+                                      src={commenter.imageUrl}
+                                      alt={commenter.name} />
+                              </dd>
+                          ))}
+                      </div>
+                      <div className="flex w-16 gap-x-2.5">
+                          <dt>
+                              <span className="sr-only">Total comments</span>
+                              {discussion.status === 'resolved' ? (
+                                  <CheckCircleIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
+                              ) : (
+                                  <ChatBubbleLeftIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
+                              )}
+                          </dt>
+                          <dd className="text-sm leading-6 text-gray-900">{discussion.totalComments}</dd>
+                      </div>
+                  </dl>
+              </li>
+          ))}
+      </ul></>
+  )
 }
-
