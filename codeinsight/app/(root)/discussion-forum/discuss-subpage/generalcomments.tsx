e3ec0a3f4c527c1cdd622cea3174e5cc3729fa85
JeromeWJQ <132572172+JeromeWJQ@users.noreply.github.com>
2024-04-16 23:01:40 +0930

    create add issue part

--- codeinsight/app/(root)/discussion-forum/discuss-subpage/generalcomments.tsx
+++ codeinsight/app/(root)/discussion-forum/discuss-subpage/generalcomments.tsx
@@ -6,6 +6,7 @@ import {
     ReceiptRefundIcon,
     UsersIcon,
   } from '@heroicons/react/24/outline'
+import { useEffect, useState } from 'react'
   
   const actions = [
     {
--- codeinsight/app/(root)/discussion-forum/page.tsx
+++ codeinsight/app/(root)/discussion-forum/page.tsx
@@ -1,17 +1,18 @@
 import CodeBox from "../submits/codebox";
 import GeneralComments from "./discuss-subpage/generalcomments";
 import Issue from "./discuss-subpage/issuepage";
-
+import { AddIssue } from "./discuss-subpage/addissue";
 
 export default function Home() {
+    
 
     return(
         
         <div className="text-sm text-indigo-500 font-medium hover:underline" >
             <CodeBox />
             <Issue />
+            <AddIssue />
             <GeneralComments />
-        
         </div>
     )
 
