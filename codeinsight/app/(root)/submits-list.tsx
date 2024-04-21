2f39a3171e9e78eb48078c6b6626839941c9406f
shiwen cao <126846768+Shiwen-Cao@users.noreply.github.com>
2024-04-21 23:52:00 +0930

    add submit-pagination component,link to submit-list file, add more sample in getAllSubmits to show the page numbers

--- codeinsight/app/(root)/submits-list.tsx
+++ codeinsight/app/(root)/submits-list.tsx
@@ -1,9 +1,13 @@
+'use client';
+
 {/*
 Note: This code includes an example of how to fetch data from an external JSON file that is hosted at https://raw.githubusercontent.com/cruip/cruip-dummy/main/job-board-posts.json. To facilitate this, we've included a lib directory in the root which contains a function that can fetch the JSON content. Additionally, we've defined the Post types in the types.d.ts file located in the root.
 */}
 
 import getAllPosts from '@/lib/getAllSubmits'
 import PostItem from './submit-item'
+import { useEffect, useState } from 'react';
+import Pagination from './submit-pagination';
 
 interface Post {
   id: number,
@@ -15,24 +19,45 @@ interface Post {
   numberReply: string,  
 }
 
-export default async function SubmitList() {
-  const postsData: Promise<Post[]> = getAllPosts()
-  const posts = await postsData
+export default function SubmitList() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 10;
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const postsData = await getAllPosts();
+      setPosts(postsData);
+    };
+
+    fetchPosts();
+  }, []);
+
+  const totalPages = Math.ceil(posts.length / postsPerPage);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
 
   return (
     <div className="pb-8 md:pb-16">
       <h2 className="text-3xl font-bold font-inter mb-10">Discuss Your Code Samples</h2>
       {/* List container */}
       <div className="flex flex-col">
-
-        {posts.map(post => {
-          return (
-            <PostItem key={post.id} {...post} />
-          )
-        })}
-
-
+        {currentPosts.map((post) => (
+          <PostItem key={post.id} {...post} />
+        ))}
       </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
     </div>
   )
 }
