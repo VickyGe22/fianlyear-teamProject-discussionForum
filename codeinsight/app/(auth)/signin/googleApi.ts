7441937a41a2fd3d3fbd3c9e8ac72f6aa9b01e45
JeromeWJQ <132572172+JeromeWJQ@users.noreply.github.com>
2024-05-13 21:57:27 +0930

    user logout
    111

--- codeinsight/app/(auth)/signin/googleApi.ts
+++ codeinsight/app/(auth)/signin/googleApi.ts
@@ -3,7 +3,7 @@
 // 设置你的 Google OAuth 2.0 客户端 ID
 const CLIENT_ID = '916055987310-mtah7asr4ld8l0m91475h9aptnei2m1e.apps.googleusercontent.com';
 // 设置你的重定向 URL
-const REDIRECT_URI = 'http://localhost:3000';
+const REDIRECT_URI = 'http://localhost:3000/userLogin';
 
 // 生成 Google API 登录链接
 export function generateGoogleLoginLink(): string {
