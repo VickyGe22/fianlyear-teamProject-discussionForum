d4920c9ee07d2ab9ef4e05ff034d325511055f77
VickyGe22 <zhige.zhang@student.adelaide.edu.au>
2024-05-26 22:29:01 +0930

    solve attribution error

--- codeinsight/app/layout.tsx
+++ codeinsight/app/layout.tsx
@@ -1,6 +1,7 @@
 import './css/style.css'
 
 import { Inter, Nothing_You_Could_Do } from 'next/font/google'
+import ClientSideEffect from './ClientSideEffect';
 
 
 
@@ -27,10 +28,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  
   return (
 
     <html lang="en">
       <body className={`${inter.variable} ${nycd.variable} font-inter antialiased bg-white text-gray-800 tracking-tight`}>
+        <ClientSideEffect />
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
           {children}
         </div>
